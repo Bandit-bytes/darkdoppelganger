f731d815998aa7ee3acd51136b466bad60ae6da4
Alshanex <discordpermacc02@gmail.com>
2025-05-17 14:45:39 +0200

    Fixed daggers orientation

--- src/main/java/net/bandit/darkdoppelganger/entity/EnderDaggerModel.java
+++ src/main/java/net/bandit/darkdoppelganger/entity/EnderDaggerModel.java
@@ -1,13 +1,17 @@
 package net.bandit.darkdoppelganger.entity;
 
 import io.redspace.ironsspellbooks.IronsSpellbooks;
+import io.redspace.ironsspellbooks.entity.mobs.abstract_spell_casting_mob.AbstractSpellCastingMob;
+import io.redspace.ironsspellbooks.render.RenderHelper;
+import net.minecraft.client.renderer.RenderType;
 import net.minecraft.resources.ResourceLocation;
 import software.bernie.geckolib.model.GeoModel;
+
+import javax.annotation.Nullable;
 
 public class EnderDaggerModel extends GeoModel<EnderDaggerEntity> {
     public static final ResourceLocation TEXTURE = ResourceLocation.fromNamespaceAndPath(IronsSpellbooks.MODID, "textures/entity/summoned_weapons/summoned_sword.png");
     public static final ResourceLocation MODEL = ResourceLocation.fromNamespaceAndPath(IronsSpellbooks.MODID, "geo/summoned_sword.geo.json");
-    public static final ResourceLocation ANIMATIONS = ResourceLocation.fromNamespaceAndPath(IronsSpellbooks.MODID, "animations/summoned_weapon_animations.json");
 
     @Override
     public ResourceLocation getModelResource(EnderDaggerEntity animatable) {
@@ -21,6 +25,11 @@ public class EnderDaggerModel extends GeoModel<EnderDaggerEntity> {
 
     @Override
     public ResourceLocation getAnimationResource(EnderDaggerEntity animatable) {
-        return ANIMATIONS;
+        return AbstractSpellCastingMob.animationInstantCast;
+    }
+
+    @Override
+    public @Nullable RenderType getRenderType(EnderDaggerEntity animatable, ResourceLocation texture) {
+        return RenderHelper.CustomerRenderType.magic(TEXTURE);
     }
 }
--- src/main/java/net/bandit/darkdoppelganger/entity/EnderDaggerRenderer.java
+++ src/main/java/net/bandit/darkdoppelganger/entity/EnderDaggerRenderer.java
@@ -3,11 +3,10 @@ package net.bandit.darkdoppelganger.entity;
 import com.mojang.blaze3d.vertex.PoseStack;
 import com.mojang.blaze3d.vertex.VertexConsumer;
 import com.mojang.math.Axis;
-import io.redspace.ironsspellbooks.render.RenderHelper;
 import net.minecraft.client.renderer.MultiBufferSource;
-import net.minecraft.client.renderer.RenderType;
 import net.minecraft.client.renderer.entity.EntityRendererProvider;
-import net.minecraft.resources.ResourceLocation;
+import net.minecraft.util.Mth;
+import net.minecraft.world.phys.Vec3;
 import software.bernie.geckolib.cache.object.BakedGeoModel;
 import software.bernie.geckolib.renderer.GeoEntityRenderer;
 import software.bernie.geckolib.util.Color;
@@ -22,17 +21,16 @@ public class EnderDaggerRenderer extends GeoEntityRenderer<EnderDaggerEntity> {
     @Override
     public void preRender(PoseStack poseStack, EnderDaggerEntity entity, BakedGeoModel model, @Nullable MultiBufferSource bufferSource, @Nullable VertexConsumer buffer, boolean isReRender, float partialTicks, int packedLight, int packedOverlay, int colour) {
         super.preRender(poseStack, entity, model, bufferSource, buffer, isReRender, partialTicks, packedLight, packedOverlay, colour);
+        Vec3 motion = entity.deltaMovementOld.add(entity.getDeltaMovement().subtract(entity.deltaMovementOld).scale(partialTicks));
+        float xRot = ((float) (Mth.atan2(motion.horizontalDistance(), motion.y) * (double) (180F / (float) Math.PI)) - 90.0F);
+        float yRot = -((float) (Mth.atan2(motion.z, motion.x) * (double) (180F / (float) Math.PI)) - 90.0F);
         poseStack.translate(0, entity.getBbHeight() * .5f, 0);
-        poseStack.mulPose(Axis.XP.rotationDegrees(-90));
-    }
-
-    @Override
-    public @Nullable RenderType getRenderType(EnderDaggerEntity animatable, ResourceLocation texture, @Nullable MultiBufferSource bufferSource, float partialTick) {
-        return RenderHelper.CustomerRenderType.magic(texture);
+        poseStack.mulPose(Axis.YP.rotationDegrees(yRot));
+        poseStack.mulPose(Axis.XP.rotationDegrees(xRot));
     }
 
     @Override
     public Color getRenderColor(EnderDaggerEntity animatable, float partialTick, int packedLight) {
-        return Color.MAGENTA;
+        return Color.LIGHT_GRAY;
     }
 }
